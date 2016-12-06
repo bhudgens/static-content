1a432e618ed1c86b2fc1e9d0dd7f1ce5d09ec417
Benjamin Hudgens <benjamin@benjamindavid.com>
2016-12-06 12:11:35 -0600

    Fix my comment header

--- server.js
+++ server.js
@@ -21,13 +21,17 @@ const express = require('express');
 const app = express();
 
 /********************************************************************
- * Route Handlers
+ * Guards
  ********************************************************************/
 
 if (!config.staticContentDirectory) {
   log.error("Static Content Directory Not Defined");
   process.exit(1);
 }
+
+/********************************************************************
+ * Route Handlers
+ ********************************************************************/
 
 // Add paths for the docs to be served via HTML
 app.use('/', express.static(`${config.staticContentDirectory}`));
